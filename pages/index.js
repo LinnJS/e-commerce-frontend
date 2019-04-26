8828caaef1a922f2d2b8207d072bd4d21a835e75
Justin Linn <justinslinn727@gmail.com>
2019-04-26 18:32:22 -0400

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Home = props => (
+  <div>
+    <span>Home</span>
+  </div>
+);
+
+export default Home;
---
+++ pages/sell.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Sell = props => (
+  <div>
+    <span>Sell</span>
+  </div>
+);
+
+export default Sell;
